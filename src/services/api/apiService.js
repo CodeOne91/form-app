c837c6b42b057259f51be0a6d7d347d50ba34741
kvothe <s.mule@ack-systems.com>
2022-07-10 01:30:54 +0200

    Ref #5 Manage of common response code:
    - add react-toastify dependency;
    - add base management of response code 200, 201, 400, 401 422;

--- src/services/api/apiService.js
+++ src/services/api/apiService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import {BASE_URL} from "../../constants/api/api";
+import {toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const baseHeaders = {
     Accept: 'application/json'
@@ -11,20 +13,46 @@ export const axiosInstance = axios.create({
 })
 
 axiosInstance.interceptors.request.use(
-    (request)=>{
-        if(request.method === 'POST' || request.method === 'post'){
+    (request) => {
+        if (request.method === 'POST' || request.method === 'post') {
             request.headers.Authorization = `Bearer ${process.env.REACT_APP_GOREST_AUTHORIZATION_TOKEN}`;
             return request
         }
         return request
     }
-
 )
 
 axiosInstance.interceptors.response.use(
-    (response)=> response,
-    ()=> Promise.reject()
+    (response) => {
+        switch (response.status) {
+            case 200:
+                toast.success('Success')
+                break
+            case 201:
+                toast.success('User creation success')
+                break
 
+        }
+        return response
+    },
+    (err) => {
+        switch (err.response.status) {
+            case 400:
+                toast.error('Bad request')
+                throw ('Bad request :' + err.message)
+                break
+            case 401:
+                toast.error('Not authorized')
+                throw ('Not Authorized :' + err.message)
+                break
+            case 422:
+                toast.error(' Data validation failed')
+                throw (' Data validation failed :' + err.message)
+                break
+            default:
+                console.log('Generic failed')
 
+        }
 
+    }
 )
