67f243b7eeb2df74d24bad8a5da69218af018e74
kvothe <s.mule@ack-systems.com>
2022-07-10 01:47:22 +0200

    Close #2 get user by id:
    - add id field

--- src/services/api/users/getUser.js
+++ src/services/api/users/getUser.js
@@ -3,8 +3,8 @@ import {USER_PATH} from "../../../constants/api/api";
 import {setUser} from "../../../store/reducers/userSlice";
 
 
-const getUser = async (dispatch)=>{
-    const response = await axiosInstance.get(USER_PATH).catch((err)=>{
+const getUser = async (id, dispatch)=>{
+    const response = await axiosInstance.get(USER_PATH+id).catch((err)=>{
         console.log('GET USER ERROR: ' + err)
         throw err;
     })
